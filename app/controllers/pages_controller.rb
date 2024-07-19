36ff7c0a6af3716c6052efeff208c4a828e8aa53
Aptitudeland <jb.garnier@gmail.com>
2024-07-19 11:33:15 +0200

    Added IF statement in the dashboard method to ensure user is connected and deal with the case the user is not

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,9 +10,13 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    @extractions = current_user.extractions.order(created_at: :desc).limit(3)
-    @user_coffees = current_user.user_coffees.where(archived: false).order(created_at: :desc).limit(3)
-    @brewing_methods = current_user.accessories.where(accessory_type: Accessory::BREWING_METHODS).last(3).reverse
-    @other_accessories = current_user.accessories.where.not(accessory_type: Accessory::BREWING_METHODS).last(5).reverse
+    if current_user
+      @extractions = current_user.extractions.order(created_at: :desc).limit(3)
+      @user_coffees = current_user.user_coffees.where(archived: false).order(created_at: :desc).limit(3)
+      @brewing_methods = current_user.accessories.where(accessory_type: Accessory::BREWING_METHODS).last(3).reverse
+      @other_accessories = current_user.accessories.where.not(accessory_type: Accessory::BREWING_METHODS).last(5).reverse
+    else
+      redirect_to new_user_session_path, alert: "Please sign in to access your dashboard."
+    end
   end
 end
